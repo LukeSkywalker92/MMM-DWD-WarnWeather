4bf3244ca764980cff96ab79a5d6b00ceeb394fd
Lukas Scheffler <luke@Lukass-MacBook-Pro.local>
2016-11-10 15:23:55 +0100

    some changes

--- MMM-DWD-WarnWeather.js
+++ MMM-DWD-WarnWeather.js
@@ -1,9 +1,11 @@
 Module.register("MMM-DWD-WarnWeather",{
     // Default module config.
     defaults: {
-        text: "Test!",
-		testnotification: "Testnachricht",
-		testpayload: "TestPayload"
+		region: 'Kreis Lörrach',
+		interval:60000, //milliseconds
+		changeColor: false,
+		loadingText: 'Warnungen werden geladen...',
+		noWarningText: 'Keine Warnungen'
     },
 
 	getScripts: function () {
@@ -15,16 +17,52 @@ Module.register("MMM-DWD-WarnWeather",{
 	},
 
 	start: function () {
-		this.sendSocketNotification("TestNotification", "TestPayload");
-		console.log('modul gestartet')
+		this.loaded = false;
+		this.noWarnings = false;
+		this.warnings = [];
+		this.updateWarnings(this);
+	},
+
+	updateWarnings: function (self) {
+		self.sendSocketNotification('GET_WARNINGS', self.config.region);
+		setTimeout(self.updateWarnings, self.config.interval, self);
 	},
 
     // Override dom generator.
     getDom: function() {
         var wrapper = document.createElement("div");
-		var icon = document.createElement("i");
-		icon.classList.add('sprite', 'sprite-binnen');
-        wrapper.appendChild(icon);
+		if (!this.loaded) {
+			wrapper.innerHTML = this.config.loadingText;
+			return wrapper;
+		}
+
+		/*if (this.noWarnings) {
+			wrapper.innerHTML = this.config.noWarningText;
+			return wrapper;
+		}*/
+
+		var icon1 = document.createElement("i");
+		icon1.classList.add('sprite', 'sprite-sturm', 'red', 'small-icon');
+		var icon2 = document.createElement("i");
+		icon2.classList.add('sprite', 'sprite-sturm', 'red', 'small-icon');
+        wrapper.appendChild(icon1);
+		wrapper.appendChild(icon2);
+
         return wrapper;
-    }
+    },
+
+	socketNotificationReceived: function (notification, payload) {
+			Log.info(notification);
+		if (notification === 'WARNINGS_DATA') {
+			Log.info(payload);
+			this.warnings = payload;
+			this.loaded = true;
+			this.noWarnings = false;
+			this.updateDom(1000);
+		} else if (notification === 'NO_WARNINGS') {
+			this.loaded = true;
+			this.noWarnings = true;
+			this.updateDom(1000);
+		}
+	}
 });
--- node_helper.js
+++ node_helper.js
@@ -6,11 +6,53 @@ module.exports = NodeHelper.create({
 		console.log('MMM-DWD-WarnWeather helper started...')
 	},
 
-	getWarningData: function () {
-		//TODO
+	getWarningData: function (region) {
+		var self = this;
+		var curl = new Curl();
+		var timestamp = Date.now().toString();
+		var url = 'http://www.dwd.de/DWD/warnungen/warnapp_landkreise/json/warnings.json?jsonp=loadWarnings' + timestamp;
+
+		curl.setOpt('URL', url);
+		curl.setOpt('FOLLOWLOCATION', true);
+
+		curl.on('end', function (statusCode, body, headers) {
+
+			var result = JSON.parse(body.substr(24).slice(0, -2));
+			for (var regionId in result['warnings']) {
+				if (result['warnings'][regionId][0]['regionName'] == region) {
+					var warnings = [];
+					for (var warning in result['warnings'][regionId]) {
+						warnings.push(result['warnings'][regionId][warning]);
+					}
+					if (warnings.length > 0) {
+						self.sendSocketNotification('WARNINGS_DATA', warnings);
+					}
+					console.log(warnings);
+				} else {
+						self.sendSocketNotification('NO_WARNINGS', 'no_warnings');
+					}
+			}
+
+
+			console.info(statusCode);
+			console.info('---');
+			//console.info(body);
+			console.info('---');
+			console.info(this.getInfo('TOTAL_TIME'));
+
+
+			this.close();
+		});
+
+		curl.on('error', curl.close.bind(curl));
+		curl.perform();
 	},
 
 	socketNotificationReceived: function (notification, payload) {
+		if (notification === 'GET_WARNINGS') {
+			this.getWarningData(payload);
+		}
+
 		console.log(notification);
 		console.log(payload);
 	}
