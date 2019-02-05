c307950f54e7e9de89da253143611a18652f971c
Josef Spitzlberger <38983450+spitzlbergerj@users.noreply.github.com>
2019-02-05 15:57:46 +0100

    Update MMM-DWD-WarnWeather.js
    extend sendSocketNotification with parameter severityThreshold

--- MMM-DWD-WarnWeather.js
+++ MMM-DWD-WarnWeather.js
@@ -58,10 +58,11 @@ Module.register("MMM-DWD-WarnWeather", {
 	// Make node_helper to get new warning-data
 	updateWarnings: function (self) {
 		if (self.config.region) {
-		  self.sendSocketNotification('GET_WARNINGS', self.config.region);
+			var region = {reg:self.config.region, sevThres:self.config.severityThreshold};
+			self.sendSocketNotification('GET_WARNINGS', region);
 	  }
 		else if (self.config.lat && self.config.lng) {
-			var coords = {lat:self.config.lat, lng:self.config.lng};
+			var coords = {lat:self.config.lat, lng:self.config.lng, sevThres:self.config.severityThreshold};
 			self.sendSocketNotification('GET_WARNINGS', coords);
 		}
 		setTimeout(self.updateWarnings, self.config.interval, self);
